2d6558d4655edbb31af8cd2a0838ad468435f878
Srinivasa Rao THANDRA <thandra82@gmail.com>
2022-12-22 16:26:31 -0800

    added changes

--- aws-s3-example/main.tf
+++ aws-s3-example/main.tf
@@ -1,11 +1,10 @@
 provider "aws" {
   profile = "default"
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "tf_course" {
   bucket = "thandra2809-test-2"
-  acl = "private"
 }
 
 resource "aws_s3_bucket_acl" "tf_course_acl" {
